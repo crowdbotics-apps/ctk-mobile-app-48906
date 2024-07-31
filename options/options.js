d46f823874070b5bd6d9c112dfd9a52648bf6e6d
Crowdbotics <team@crowdbotics.com>
2024-07-31 10:15:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ctk-mobile-app-48906",
+  url: "https://ctk-mobile-app-48906.botics.co",
+  api: "https://ctk-mobile-app-48906.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
